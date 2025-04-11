b94c857d0f3951e7fe4211ef9a4918c1d9f2dd2f
akashpande1886 <72142288+akashpande1886@users.noreply.github.com>
2025-04-11 12:58:28 +0530

    async await added

--- src/controllers/host.controller.js
+++ src/controllers/host.controller.js
@@ -1,18 +1,18 @@
 const Home = require("../models/home.model");
 
-// const registeredHomes = [];
-
-exports.getAddHome = (req, res, next) => {
+exports.getAddHome = async (req, res, next) => {
   res.render("host/edit-home", {
     pageTitle: "Add Home",
     currentPage: "addHome",
     editing: false,
   });
 };
-exports.getEditHome = (req, res, next) => {
+
+exports.getEditHome = async (req, res, next) => {
   const homeId = req.params.homeId;
   const editing = req.query.editing === "true";
-  Home.findById(homeId).then((home) => {
+  try {
+    const home = await Home.findById(homeId);
     if (!home) {
       console.log("Home not found for editing");
       return res.redirect("/host/host-home-list");
@@ -24,55 +24,78 @@ exports.getEditHome = (req, res, next) => {
       currentPage: "host-homes",
       editing: editing,
     });
-  });
+  } catch (err) {
+    console.log("Error while finding home", err);
+    // Consider rendering an error page or redirecting with an error message
+    res.redirect("/host/host-home-list");
+  }
 };
-exports.postAddHome = (req, res, next) => {
+
+exports.postAddHome = async (req, res, next) => {
   const { houseName, price, location, rating, imageUrl } = req.body;
   const home = new Home({ houseName, price, location, rating, imageUrl });
-  home.save();
-
-  res.redirect("/host/host-home-list");
+  try {
+    await home.save();
+    res.redirect("/host/host-home-list");
+  } catch (err) {
+    console.log("Error while saving home", err);
+    // Consider rendering an error page or redirecting with an error message
+    res.redirect("/host/add-home"); // Or wherever the form is
+  }
 };
-exports.getHostHomes = (req, res, next) => {
-  Home.find().then((registeredHomes) => {
+
+exports.getHostHomes = async (req, res, next) => {
+  try {
+    const registeredHomes = await Home.find();
     res.render("host/host-home-list", {
       registeredHomes: registeredHomes,
       pageTitle: "airbnb Home",
       currentPage: "index",
     });
-  });
-};
-exports.postEditHome = (req, res, next) => {
-  const { id, houseName, price, location, rating, imageUrl } = req.body;
-  Home.findById(id)
-    .then((home) => {
-      home.houseName = houseName;
-      home.price = price;
-      home.location = location;
-      home.rating = rating;
-      home.imageUrl = imageUrl;
-      home
-        .save()
-        .then((result) => {
-          console.log("Home updated", result);
-        })
-        .catch((err) => {
-          console.log("Error while updating", err);
-        });
-      res.redirect("/host/host-home-list");
-    })
-    .catch((err) => {
-      console.log("Error while finding home", err);
+  } catch (err) {
+    console.log("Error while fetching homes", err);
+    // Consider rendering an error page or displaying an error message
+    res.render("host/host-home-list", {
+      registeredHomes: [],
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+      errorMessage: "Failed to load homes.",
     });
+  }
 };
-exports.postDeleteHome = (req, res, next) => {
+
+exports.postEditHome = async (req, res, next) => {
+  const { id, houseName, price, location, rating, imageUrl } = req.body;
+  try {
+    const home = await Home.findById(id);
+    if (!home) {
+      console.log("Home not found for updating");
+      return res.redirect("/host/host-home-list");
+    }
+    home.houseName = houseName;
+    home.price = price;
+    home.location = location;
+    home.rating = rating;
+    home.imageUrl = imageUrl;
+    const result = await home.save();
+    console.log("Home updated", result);
+    res.redirect("/host/host-home-list");
+  } catch (err) {
+    console.log("Error while updating home", err);
+    // Consider rendering an error page or redirecting with an error message
+    res.redirect(`/host/edit-home/${id}?editing=true`); // Redirect back to edit form with error
+  }
+};
+
+exports.postDeleteHome = async (req, res, next) => {
   const homeId = req.params.homeId;
   console.log("came to delete home id", homeId);
-  Home.findByIdAndDelete(homeId)
-    .then(() => {
-      res.redirect("/host/host-home-list");
-    })
-    .catch((err) => {
-      console.log("Error while finding home", err);
-    });
+  try {
+    await Home.findByIdAndDelete(homeId);
+    res.redirect("/host/host-home-list");
+  } catch (err) {
+    console.log("Error while deleting home", err);
+    // Consider redirecting back with an error message
+    res.redirect("/host/host-home-list");
+  }
 };
--- src/controllers/store.controller.js
+++ src/controllers/store.controller.js
@@ -1,92 +1,114 @@
 const Favourite = require("../models/fav.model");
 const Home = require("../models/home.model");
 
-// const registeredHomes = [];
-
-exports.getIndex = (req, res, next) => {
-  Home.find().then((registeredHomes) => {
+exports.getIndex = async (req, res, next) => {
+  try {
+    const registeredHomes = await Home.find();
     res.render("store/index", {
       registeredHomes: registeredHomes,
       pageTitle: "airbnb Home",
       currentPage: "index",
     });
-  });
+  } catch (err) {
+    console.log("Error fetching homes for index:", err);
+    res.render("store/index", {
+      registeredHomes: [],
+      pageTitle: "airbnb Home",
+      currentPage: "index",
+      errorMessage: "Failed to load homes.",
+    });
+  }
 };
-exports.getHome = (req, res, next) => {
-  Home.find().then((registeredHomes) => {
+
+exports.getHome = async (req, res, next) => {
+  try {
+    const registeredHomes = await Home.find();
     res.render("store/home", {
       registeredHomes: registeredHomes,
       pageTitle: "airbnb Home",
       currentPage: "home",
     });
-  });
+  } catch (err) {
+    console.log("Error fetching homes for home page:", err);
+    res.render("store/home", {
+      registeredHomes: [],
+      pageTitle: "airbnb Home",
+      currentPage: "home",
+      errorMessage: "Failed to load homes.",
+    });
+  }
 };
-exports.getBookings = (req, res, next) => {
+
+exports.getBookings = async (req, res, next) => {
   res.render("store/bookings", {
     pageTitle: "My Bookings",
     currentPage: "bookings",
   });
 };
-exports.getFavouriteList = (req, res, next) => {
-  Favourite.find()
-    .populate("homeId")
-    .then((favourites) => {
-      const favouriteHomes = favourites.map((favourite) => favourite.homeId);
 
-      res.render("store/favourites", {
-        favHomes: favouriteHomes,
-        pageTitle: "My favourites",
-      });
+exports.getFavouriteList = async (req, res, next) => {
+  try {
+    const favourites = await Favourite.find().populate("homeId");
+    const favouriteHomes = favourites.map((favourite) => favourite.homeId);
+    res.render("store/favourites", {
+      favHomes: favouriteHomes,
+      pageTitle: "My favourites",
     });
+  } catch (err) {
+    console.log("Error fetching favourites:", err);
+    res.render("store/favourites", {
+      favHomes: [],
+      pageTitle: "My favourites",
+      errorMessage: "Failed to load your favourite homes.",
+    });
+  }
 };
-exports.postAddToFavouriteList = (req, res, next) => {
+
+exports.postAddToFavouriteList = async (req, res, next) => {
   const homeId = req.body.id;
-  Favourite.findOne({ homeId: homeId })
-    .then((existingFav) => {
-      if (existingFav) {
-        return res.redirect("/favourites");
-      }
-      const fav = new Favourite({ homeId: homeId });
-      return fav.save();
-    })
-    .then(() => {
-      res.redirect("/favourites");
-    })
-    .catch((err) => {
-      console.log("Error while adding to favourites", err);
-    });
+  try {
+    const existingFav = await Favourite.findOne({ homeId: homeId });
+    if (existingFav) {
+      return res.redirect("/favourites");
+    }
+    const fav = new Favourite({ homeId: homeId });
+    await fav.save();
+    res.redirect("/favourites");
+  } catch (err) {
+    console.log("Error while adding to favourites:", err);
+    res.redirect("/"); // Or wherever the user came from, with an error message
+  }
 };
 
-exports.postRemoveFromFavouriteList = (req, res, next) => {
+exports.postRemoveFromFavouriteList = async (req, res, next) => {
   const homeId = req.params.homeId;
-  console.log("came to reove fav", homeId);
-  Favourite.findOneAndDelete({ homeId: homeId })
-    .then(() => {
-      res.redirect("/favourites");
-    })
-    .catch(() => {
-      console.log("error while removing from favourites", err);
-    });
+  console.log("came to remove fav", homeId);
+  try {
+    await Favourite.findOneAndDelete({ homeId: homeId });
+    res.redirect("/favourites");
+  } catch (err) {
+    console.log("Error while removing from favourites:", err);
+    res.redirect("/favourites"); // Or back to the favourites page with an error message
+  }
 };
-exports.getHomeDetails = (req, res, next) => {
+
+exports.getHomeDetails = async (req, res, next) => {
   const homeId = req.params.homeId;
   console.log("At home details page:", homeId);
-  //  console.log(registeredHomes)
-
-  Home.findById(homeId).then((home) => {
+  try {
+    const home = await Home.findById(homeId);
     if (!home) {
       console.log("Home not found!");
-      res.redirect("/index");
-    } else {
-      console.log("home details found", home);
-      res.render("store/home-details", {
-        // registeredHomes: registeredHomes(),
-        home: home,
-        pageTitle: "home details",
-        currentPage: "home",
-      });
+      return res.redirect("/index");
     }
-  });
+    console.log("home details found", home);
+    res.render("store/home-details", {
+      home: home,
+      pageTitle: "home details",
+      currentPage: "home",
+    });
+  } catch (err) {
+    console.log("Error fetching home details:", err);
+    res.redirect("/index"); // Redirect to index with an error message if needed
+  }
 };
-
-// exports.registeredHomes = registeredHomes;
